27eb6bbdf3e5601dd0007739c6fe3024c1016231
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2021-05-30 00:34:29 -0700

    [fizzbuzz] add tests for fizz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,3 +5,9 @@ def test_numbers():
     assert fizzbuzz(2) == "2"
     assert fizzbuzz(4) == "4"
     assert fizzbuzz(101) == "101"
+
+def test_fizz():
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(6) == "Fizz"
+    assert fizzbuzz(9) == "Fizz"
+    assert fizzbuzz(99) == "Fizz"
