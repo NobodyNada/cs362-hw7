08c3249711e7d442fd97099d4dc2b671f23ff313
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2021-05-30 00:46:37 -0700

    [leapyear] add tests for multiples of 4

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,3 +5,8 @@ def test_normal_years():
     assert not is_leap_year(2001)
     assert not is_leap_year(2017)
     assert not is_leap_year(2021)
+
+def test_multiples_of_4():
+    assert is_leap_year(4)
+    assert is_leap_year(2008)
+    assert is_leap_year(2020)
